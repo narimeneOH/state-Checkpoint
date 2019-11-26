ccb18242f330a664a95559658dc5df97b93e951b
narimeneOH <ouhidanarimene8@gmail.com>
2019-11-26 14:41:51 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Photo = (props) => {
+  return <img src={props.src} alt=""></img>;
+};
+
+
+export default class App  extends React.Component{
+ 
+}
+
---
+++ src/componenets.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Photo = (props) => {
+  return <img src={props.src} alt=""></img>;
+};
+
+
+export default  Button = (props){
+ return (
+     <button onClick={
+
+
+
+    }
+ )
+}
+
